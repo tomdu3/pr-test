642ff3e63b09012aa88b3357dcf3ddd331f455df
Tomislav Dukez <tomdu3@ymail.com>
2024-05-26 17:32:56 +0200

    Add first run

--- test.py
+++ test.py
@@ -10,27 +10,26 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 
-print(API_KEY)
-# url = "https://imdb146.p.rapidapi.com/v1/find/"
+url = "https://imdb146.p.rapidapi.com/v1/find/"
 
-# querystring = {"query": "Die Hard"}
+querystring = {"query": "Die Hard"}
 
-# headers = {
-# 	"X-RapidAPI-Key": API_KEY,
-# 	"X-RapidAPI-Host": "imdb146.p.rapidapi.com"
-# }
+headers = {
+	"X-RapidAPI-Key": API_KEY,
+	"X-RapidAPI-Host": "imdb146.p.rapidapi.com"
+}
 
-# response = requests.get(url, headers=headers, params=querystring)
+response = requests.get(url, headers=headers, params=querystring)
 
-# print(response.json())
+print(response.json())
 
 # save response to a local json file
-# with open('imdb.json', 'w') as f:
-#     json.dump(response.json(), f)
+with open('imdb.json', 'w') as f:
+    json.dump(response.json(), f)
 
-# titles = [title for title in response.json()['titleResults']['results']]
+titles = [title for title in response.json()['titleResults']['results']]
 
-# titles_sorted = [title['titleNameText'] for title in sorted(titles, key=lambda x: x.get('titleReleaseText', 0))]  # nice example of use of lambda
+titles_sorted = [title['titleNameText'] for title in sorted(titles, key=lambda x: x.get('titleReleaseText', 0))]  # nice example of use of lambda
 
 
-# pprint(titles_sorted)
+pprint(titles_sorted)
